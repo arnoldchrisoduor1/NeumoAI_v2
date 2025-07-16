26cc34ff68491e9f2d9792efbcc6a416f008d835
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2025-07-16 17:36:03 +0300

    updating the routes

--- neumo_api/lib/neumo_api_web/router.ex
+++ neumo_api/lib/neumo_api_web/router.ex
@@ -1,28 +1,35 @@
 defmodule NeumoApiWeb.Router do
   use NeumoApiWeb, :router
 
+  import Phoenix.Controller
+  import Guardian.Plug
+
   pipeline :api do
     plug :accepts, ["json"]
   end
 
-  scope "/api", NeumoApiWeb do
-    pipe_through :api
+  # Pipeline for the authenticated API requests.
+  pipeline :api_auth do
+    plug :accepts, ["json"]
+    plug Guardian.Plug.VerifyHeader, claims: %{"type" => "access"}
+    plug Guardian.Plug.LoadResource
+    plug Guardian.Plug.EnsureAuthenticated
   end
 
-  # Enable LiveDashboard and Swoosh mailbox preview in development
-  if Application.compile_env(:neumo_api, :dev_routes) do
-    # If you want to use the LiveDashboard in production, you should put
-    # it behind authentication and allow only admins to access it.
-    # If your application does not have an admins-only section yet,
-    # you can use Plug.BasicAuth to set up some basic authentication
-    # as long as you are also using SSL (which you should anyway).
-    import Phoenix.LiveDashboard.Router
+  scope "/api", NeumoApiWeb do
+    pipe_through :api
 
-    scope "/dev" do
-      pipe_through [:fetch_session, :protect_from_forgery]
+    # user registration.
+    post "/register", RegistrationController, :create
 
-      live_dashboard "/dashboard", metrics: NeumoApiWeb.Telemetry
-      forward "/mailbox", Plug.Swoosh.MailboxPreview
+    # User session.
+    post "/login", SessionController, :create
+    delete "/logout", SessionController, :delete, as: :session
+
+    # our example protected route.
+    scope "/protected" do
+      pipe_through :api_auth
+      get "/", ProtectedContoller, :index
     end
   end
 end
