c2f006c21ae02ee5a0d3c55346912677a9135f3c
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2025-07-16 17:23:01 +0300

    creating the error views

---
+++ neumo_api/lib/neumo_api_web/views/error_view.ex
@@ -0,0 +1,31 @@
+defmodule NeumoApiWeb.ErrorView do
+  use NeumoApiWeb, :view
+
+  def render("401.json", _assigns) do
+    %{errors: [%{status: "401", title: "Unauthorized", detail: "Authentication required or invalid credentials."}]}
+  end
+
+  def render("404.json", _assigns) do
+    %{errors: [%{status: "401", title: "Not Found"}]}
+  end
+
+  def render("422.json", %{errors: changeset}) do
+    %{errors: %{
+      status: "422",
+      title: "Unprocessable Entity",
+      details: NeumoApiWeb.ErrorHelpers.translate_errors(changeset)
+    }}
+  end
+
+  def render("422.json", %{message: message}) do
+    %{errors: [%{status: "422", title: "Unprocessable Entity", detail: message}]}
+  end
+
+  def render("500.json", _assign) do
+    %{errrors: [%{status: "500", title: "Internal Server Error"}]}
+  end
+
+  def render("500.json", %{message: message}) do
+    %{errors: [%{status: "500", title: "Internal Server Error", detail: message}]}
+  end
+end
